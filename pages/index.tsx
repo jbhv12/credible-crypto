c56bc524684eb6bf3fd17e0bf200d7bff64b2b7c
Jay Bhavsar <jbhv12@gmail.com>
2024-04-05 12:28:39 +0530

    gelato integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,79 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import {
+  ThirdwebNftMedia,
+  useAddress,
+  useChainId,
+  useContract,
+  useMetamask,
+  useNFTs,
+  ConnectWallet,
+} from "@thirdweb-dev/react";
+import { GelatoRelay, CallWithERC2771Request } from "@gelatonetwork/relay-sdk";
+import { Bytes, BytesLike, ethers } from "ethers";
+import ReviewTokenABI from "../assets/abi/ReviewToken.json";
+
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import { NextPage } from "next";
 
+import { useState, useEffect } from "react";
+
 const Home: NextPage = () => {
+  const target = "0xb241B676818bd9EF4AFdb3Bc246c429aA3DF4dd1";
+  const address = useAddress();
+  const chainId = useChainId();
+  const { contract } = useContract(target, ReviewTokenABI.abi);
+  const relay = new GelatoRelay();
+  const gelatoAPI = "IeTEZaCSVQtOSQbBCnQV8JxGBJiOgH_X_bMGwOJw5uY_";
+
+  // const { data: nfts, isLoading: loading } = useNFTs(contract, {
+  //   start: 0,
+  //   count: 10,
+  // });
+
+  // console.log(contract?.call("owner", []));
+
+  const [owner, setOwner] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchOwner = async () => {
+      try {
+        const ownerResult = await contract?.call("owner", []);
+        setOwner(ownerResult);
+      } catch (error) {
+        console.error("Error fetching owner:", error);
+      }
+    };
+
+    fetchOwner();
+  }, [contract]);
+
+
+  const truncateAddress = (address: string) => {
+    return (
+      address.substring(0, 6) + "..." + address.substring(address.length - 4)
+    );
+  };
+
+  const sendRelayRequest = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum as any);
+    const signer = provider.getSigner();
+    const user =  await signer.getAddress();
+
+    const g_contract = new ethers.Contract(target, ReviewTokenABI.abi, signer);
+    const {data} =  await g_contract.populateTransaction.owner();
+
+    console.log(chainId);
+
+    const request: CallWithERC2771Request = {
+      chainId: chainId,
+      target: target,
+      data: data as BytesLike,
+      user: user
+    };
+    const relayResponse = await relay.sponsoredCallERC2771(request, provider, gelatoAPI);
+    console.log(relayResponse);
+    
+  };
   return (
     <main className={styles.main}>
       <div className={styles.container}>
@@ -28,6 +98,10 @@ const Home: NextPage = () => {
             <p>{owner}</p>
             {owner === address ? <p>true</p> : <p>false</p>}
           </div>
+
+          <button
+              onClick={sendRelayRequest}
+            ></button>
 
         </div>
       </div>
